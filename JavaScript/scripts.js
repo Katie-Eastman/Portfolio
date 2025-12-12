c4058c07d547893fbc1a6575294dd38c276c6bc1
Katie Eastman <katherineeastman@my.cwi.edu>
2025-12-11 19:51:20 -0700

    Update scripts.js

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -1,1 +1,4 @@
-
+//when the user submits the contact form, they will be thanked for their successful submission
+function confirmSubmit(){
+	alert("Thank you for contacting me! I will get back to you as soon as possible.");
+}
