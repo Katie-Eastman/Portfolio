cebd84ff142e514246fd8bd9f3e2611a87db47d7
Katie Eastman <katherineeastman@my.cwi.edu>
2026-05-14 20:07:33 -0600

    Delete JavaScript directory

--- JavaScript/scripts.js
+++
@@ -1,4 +0,0 @@
-//when the user submits the contact form, they will be thanked for their successful submission
-function confirmSubmit(){
-	alert("Thank you for contacting me! I will get back to you as soon as possible.");
-}
